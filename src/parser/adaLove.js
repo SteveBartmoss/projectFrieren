f3b0bbcc8917b29d9317f1711c78283fb89aab6a
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2026-01-08 08:35:12 -0600

    feat: Se agregaron los prototipos de algunas funciones

--- src/parser/adaLove.js
+++ src/parser/adaLove.js
@@ -2,4 +2,16 @@
 
 export class Parser{
     
+    static stackTree = []
+
+
+    static pushToken(token){
+        this.stackTree.push(token)
+    }
+
+    static processCode(){
+
+    }
+    
+
 }
