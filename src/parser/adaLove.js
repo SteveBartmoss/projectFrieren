eae78c54226eba5b9f513b8ab953b9ff3c26462c
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2026-01-19 12:54:46 -0600

    feat: Se implemento la funcion para el parseo de los tokens

--- src/parser/adaLove.js
+++ src/parser/adaLove.js
@@ -9,8 +9,53 @@ export class Parser{
         this.stackTree.push(token)
     }
 
+    static popToken(){
+        this.stackTree.pop()
+    }
+
     static processCode(){
 
+        let itarator = 0
+        let state = 0
+        let pointer = ""
+
+        while(itarator < this.stackTree.length){
+
+            pointer = this.stackTree[iterator]
+
+            switch(state){
+
+                case 0: 
+                    if(pointer=== "program"){
+                        this.popToken()
+                        this.pushToken("table_decl")
+                        state = 1
+                        itarator++
+                    }
+                break
+
+                case 1: 
+                    if(pointer === "table_decla"){
+                        this.popToken()
+                        state = 2
+                        itarator++
+                    }
+                break
+
+                case 2:
+                    if(pointer === "table"){
+                        this.popToken()
+                        state = 3
+                        itarator++
+                    }
+                    else {
+                        throw new Error("Expected 'table' keyword recibed ->",pointer)
+                    }
+                break
+
+            }
+
+        }
     }
     
 
